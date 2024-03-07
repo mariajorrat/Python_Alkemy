01a193753171429ed48f9cfaad4c886fde8ddd25
Maria Florencia Jorrat <83671274+mariajorrat@users.noreply.github.com>
2024-03-07 16:19:13 -0300

    update Cantidad_de_bancos.py

--- Clase 2/Cantidad_de_bancos.py
+++ Clase 2/Cantidad_de_bancos.py
@@ -2,13 +2,7 @@
 alumnos inscriptos para un curso, permita determinar si alcanzan los bancos existentes. 
 De  no  ser  así,  informar  además  cuantos  bancos  sería  necesario  agregar.  El  usuario 
 deberá  ingresar  por  teclado  tanto  la  cantidad  de  bancos  que  tiene  el  aula,  como  la 
-cantidad de alumnos inscriptos para el curso. 
- 
-Ejercicio 5: Cálculo del salario 
-A un trabajador le pagan según sus horas trabajadas y la tarifa está a un valor por hora. 
-Si la cantidad de horas trabajadas es mayor a 40 horas, la tarifa por hora se incrementa 
-en  un  50%  para  las  horas  extras.  Calcular  el  salario  del  trabajador  dadas  las  horas 
-trabajadas y la tarifa. '''
+cantidad de alumnos inscriptos para el curso.'''
 
 
 
